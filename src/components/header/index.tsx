b3b8716eebf3057513a683cb34a8919b3310ab39
eddyzera <nescau23041996@gmail.com>
2021-12-09 20:17:55 -0300

    [style]: creation style for version mobile

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 export const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="header__d-flex">
-        <h1>devFinder</h1>
-      </div>
+      <h1>devFinder</h1>
     </header>
   )
 }
